6b8fe2d80a9bc0bbb748b64a9132fc21a4c1049b
Steven Sewell <stevenrsewell@outlook.com>
2023-04-08 20:05:14 -0500

    Project 2

---
+++ Project 2/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt('Please enter your first name: ')
+let lastName = prompt('Please enter your last name: ')
+let age = parseInt(prompt('Please enter your age: '))
+
+age = age + 2
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
